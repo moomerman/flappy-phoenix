44d698c76198878bb8c2ffe3c98e7a3b56d0b2a1
Richard Taylor <moomerman@gmail.com>
2019-03-27 20:23:31 +0000

    Integrate tailwindcss via postcss & purgecss

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path');
-const glob = require('glob');
+const glob = require('glob-all');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const PurgecssPlugin = require('purgecss-webpack-plugin');
 
-module.exports = (env, options) => ({
+var config = {
   optimization: {
     minimizer: [
       new UglifyJsPlugin({ cache: true, parallel: true, sourceMap: false }),
@@ -13,7 +14,7 @@ module.exports = (env, options) => ({
     ]
   },
   entry: {
-      './js/app.js': ['./js/app.js'].concat(glob.sync('./vendor/**/*.js'))
+    './js/app.js': ['./js/app.js'].concat(glob.sync('./vendor/**/*.js'))
   },
   output: {
     filename: 'app.js',
@@ -30,12 +31,38 @@ module.exports = (env, options) => ({
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
       }
     ]
   },
   plugins: [
     new MiniCssExtractPlugin({ filename: '../css/app.css' }),
-    new CopyWebpackPlugin([{ from: 'static/', to: '../' }])
+    new CopyWebpackPlugin([{ from: 'static/', to: '../' }]),
   ]
-});
+}
+
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+  }
+}
+
+module.exports = (env, options) => {
+  if (options.mode == 'production') {
+    config.plugins.push(
+      new PurgecssPlugin({
+        paths: glob.sync([
+          path.join(__dirname, '../lib/flappy_phoenix_web/templates/**/*.eex'),
+          path.join(__dirname, '../lib/flappy_phoenix_web/views/*.ex'),
+        ]),
+        extractors: [
+          {
+            extractor: TailwindExtractor,
+            extensions: ["html", "js", "ex", "eex"],
+          }
+        ]
+      })
+    )
+  }
+  return config;
+}
