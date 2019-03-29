d415e684d0dcd53875d5ef7fe9b90a8937faa3c2
Richard Taylor <moomerman@gmail.com>
2019-03-29 16:29:45 +0000

    Add heroku configuration

--- config/prod.exs
+++ config/prod.exs
@@ -11,8 +11,10 @@ use Mix.Config
 # before starting your production server.
 config :flappy_phoenix, FlappyPhoenixWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [host: "flappy-phoenix.herokuapp.com", port: 443, scheme: "https"],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -65,7 +67,3 @@ config :logger, level: :info
 #
 # Note you can't rely on `System.get_env/1` when using releases.
 # See the releases documentation accordingly.
-
-# Finally import the config/prod.secret.exs which should be versioned
-# separately.
-import_config "prod.secret.exs"
--- lib/flappy_phoenix_web/endpoint.ex
+++ lib/flappy_phoenix_web/endpoint.ex
@@ -2,10 +2,10 @@ defmodule FlappyPhoenixWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :flappy_phoenix
 
   socket "/socket", FlappyPhoenixWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
-  socket "/live", Phoenix.LiveView.Socket
+  socket "/live", Phoenix.LiveView.Socket, websocket: [timeout: 45_000]
 
   # Serve at "/" the static files from "priv/static" directory.
   #
