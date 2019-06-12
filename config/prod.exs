eae76182e143d8d5824c98ede013a3d416e0115d
Richard Taylor <moomerman@gmail.com>
2019-06-12 14:12:45 +0100

    Fix prod build, missing ENV var

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ config :flappy_phoenix, FlappyPhoenixWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
   url: [host: "flappy-phoenix.herokuapp.com", port: 443, scheme: "https"],
   cache_static_manifest: "priv/static/cache_manifest.json",
-  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
+  secret_key_base: System.get_env("SECRET_KEY_BASE"), 
   force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
 # Do not print debug messages in production
